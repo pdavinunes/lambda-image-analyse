6467dc7d42d7dcabea3daa0ea29715820437c856
Davi Nunes <p.davi.nunes@gmail.com>
2021-03-22 21:58:15 -0300

    traduz labels e gera texto

--- handler.js
+++ handler.js
@@ -4,8 +4,9 @@ const {
 } = require("fs");
 
 class Handler {
-  constructor({ rekoSvc }) {
+  constructor({ rekoSvc, translatorSvc }) {
     this.rekoSvc = rekoSvc;
+    this.translatorSvc = translatorSvc;
   }
 
   async detectImageLabels(buffer) {
@@ -26,14 +27,50 @@ class Handler {
     return { names, workingItems };
   }
 
+  async translateText(text) {
+    const params = {
+      SourceLanguageCode: "en",
+      TargetLanguageCode: "pt",
+      Text: text,
+    };
+
+    const { TranslatedText } = await this.translatorSvc
+      .translateText(params)
+      .promise();
+
+    return TranslatedText.split(" e ");
+  }
+
+  formatTextResult(texts, workingItems) {
+    const finalText = [];
+    console.log("texts", texts);
+    console.log("workingItems", workingItems);
+    for (const indexText in texts) {
+      const nameInPortuguese = texts[indexText];
+      const confidence = workingItems[indexText].Confidence;
+
+      finalText.push(
+        `${confidence.toFixed(2)}% de ser do tipo ${nameInPortuguese}`
+      );
+    }
+
+    return finalText.join("\n");
+  }
+
   async main(event) {
     try {
       const imgBuffer = await readFile("./images/cat.jpg");
-      console.log("Detecting Labels...");
+      console.log("Detecting images...");
       const { names, workingItems } = await this.detectImageLabels(imgBuffer);
+      console.log("Translating to Portuguese...");
+      const texts = await this.translateText(names);
+      console.log("Handling final object...");
+      const finalText = this.formatTextResult(texts, workingItems);
+      console.log("finishing...");
+
       return {
         statusCode: 200,
-        body: { names, workingItems },
+        body: `A imagem tem\n `.concat(finalText),
       };
     } catch (error) {
       console.log("**ERROR**", error.stack);
@@ -48,8 +85,10 @@ class Handler {
 //factory
 const aws = require("aws-sdk");
 const reko = new aws.Rekognition();
+const translator = new aws.Translate();
 const handler = new Handler({
   rekoSvc: reko,
+  translatorSvc: translator,
 });
 
 module.exports.main = handler.main.bind(handler);
