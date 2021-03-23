093724fa0b3326c7505fd375b709ce8514541e34
Davi Nunes <p.davi.nunes@gmail.com>
2021-03-22 21:40:37 -0300

    detecta imagens com o AWS Rekogition

--- handler.js
+++ handler.js
@@ -1,15 +1,55 @@
 "use strict";
+const {
+  promises: { readFile },
+} = require("fs");
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
+class Handler {
+  constructor({ rekoSvc }) {
+    this.rekoSvc = rekoSvc;
+  }
+
+  async detectImageLabels(buffer) {
+    const result = await this.rekoSvc
+      .detectLabels({
+        Image: {
+          Bytes: buffer,
+        },
+      })
+      .promise();
+
+    const workingItems = result.Labels.filter(
+      ({ Confidence }) => Confidence > 80
+    );
+
+    const names = workingItems.map(({ Name }) => Name).join(" and ");
+
+    return { names, workingItems };
+  }
+
+  async main(event) {
+    try {
+      const imgBuffer = await readFile("./images/cat.jpg");
+      console.log("Detecting Labels...");
+      const { names, workingItems } = await this.detectImageLabels(imgBuffer);
+      return {
+        statusCode: 200,
+        body: { names, workingItems },
+      };
+    } catch (error) {
+      console.log("**ERROR**", error.stack);
+      return {
+        statusCode: 500,
+        body: "Internal Server Error",
+      };
+    }
+  }
+}
+
+//factory
+const aws = require("aws-sdk");
+const reko = new aws.Rekognition();
+const handler = new Handler({
+  rekoSvc: reko,
+});
+
+module.exports.main = handler.main.bind(handler);
